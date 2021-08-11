062a71d106c1894fbc5e669661346fc613d8f051
Jason <jasonyen1009@gmail.com>
2021-08-11 14:00:08 +0800

    隨機變色的小熊

--- Demo4/ViewController.swift
+++ Demo4/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var greennumber: UILabel!
+    @IBOutlet weak var bluenumber: UILabel!
+    @IBOutlet weak var alphanumber: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loveTextField: UITextField!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    @IBOutlet var viewBackground: UIView!
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func speak(_ sender: Any) {
+        let speechUtterance = AVSpeechUtterance(string: loveTextField.text!)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(speechUtterance)
+        
+    }
+    @IBAction func slider(_ sender: Any) {
+        viewBackground.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        number.text = String(format: "%.2f", redSlider.value)
+        bluenumber.text = String(format: "%.2f", blueSlider.value)
+        greennumber.text = String(format: "%.2f", greenSlider.value)
+        alphanumber.text = String(format: "%.2f", alphaSlider.value)
+        
+    }
+    @IBAction func randomnumber(_ sender: Any) {
+        redSlider.value = Float(Int.random(in: 0...100))/100
+        greenSlider.value = Float(Int.random(in: 0...100))/100
+        blueSlider.value = Float(Int.random(in: 0...100))/100
+        
+        viewBackground.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        number.text = String(format: "%.2f", redSlider.value)
+        bluenumber.text = String(format: "%.2f", blueSlider.value)
+        greennumber.text = String(format: "%.2f", greenSlider.value)
+        //alphanumber.text = String(format: "%.2f", alphaSlider.value)
+        print(greenSlider.value)
+        print(redSlider.value)
+    }
+    
 
 }
+
 
